186a3527046fde325b705ebbaf8e06a929600465
nangld <nangld290498@gmail.com>
2022-03-16 12:28:44 +0700

    Outter UI

---
+++ RadarAnalyst/RadarAnalyst/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace RadarAnalyst
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ms_mainMenu = new System.Windows.Forms.MenuStrip();
+            this.smi_vtddrd = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_kvctd = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_dcdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_mpx = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_ktmpx = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_dll = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_dn = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_mcdh = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_gck = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_pvphmp = new System.Windows.Forms.ToolStripMenuItem();
+            this.smi_help = new System.Windows.Forms.ToolStripMenuItem();
+            this.tc_mainTap = new System.Windows.Forms.TabControl();
+            
+            this.ms_mainMenu.SuspendLayout();
+            this.tc_mainTap.SuspendLayout();
+            
+            this.SuspendLayout();
+            // 
+            // ms_mainMenu
+            // 
+            this.ms_mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smi_vtddrd,
+            this.smi_mpx,
+            this.smi_mcdh,
+            this.smi_gck,
+            this.smi_pvphmp,
+            this.smi_help});
+            this.ms_mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.ms_mainMenu.Name = "ms_mainMenu";
+            this.ms_mainMenu.Size = new System.Drawing.Size(1277, 24);
+            this.ms_mainMenu.TabIndex = 0;
+            this.ms_mainMenu.Text = "Main Menu";
+            // 
+            // smi_vtddrd
+            // 
+            this.smi_vtddrd.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smi_kvctd,
+            this.smi_dcdd});
+            this.smi_vtddrd.Name = "smi_vtddrd";
+            this.smi_vtddrd.Size = new System.Drawing.Size(129, 20);
+            this.smi_vtddrd.Text = "VỊ TRÍ ĐẶT ĐÀI RADA";
+            // 
+            // smi_kvctd
+            // 
+            this.smi_kvctd.Name = "smi_kvctd";
+            this.smi_kvctd.Size = new System.Drawing.Size(179, 22);
+            this.smi_kvctd.Text = "KHU VỰC CHỌN TĐ";
+            this.smi_kvctd.Click += new System.EventHandler(this.smi_kvctd_Click);
+            // 
+            // smi_dcdd
+            // 
+            this.smi_dcdd.Name = "smi_dcdd";
+            this.smi_dcdd.Size = new System.Drawing.Size(179, 22);
+            this.smi_dcdd.Text = "ĐỘ CAO Đ. ĐÀI";
+            this.smi_dcdd.Click += new System.EventHandler(this.smi_dcdd_Click);
+            // 
+            // smi_mpx
+            // 
+            this.smi_mpx.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.smi_ktmpx,
+            this.smi_dll,
+            this.smi_dn});
+            this.smi_mpx.Name = "smi_mpx";
+            this.smi_mpx.Size = new System.Drawing.Size(98, 20);
+            this.smi_mpx.Text = "MẶT PHẢN XẠ";
+            // 
+            // smi_ktmpx
+            // 
+            this.smi_ktmpx.Name = "smi_ktmpx";
+            this.smi_ktmpx.Size = new System.Drawing.Size(172, 22);
+            this.smi_ktmpx.Text = "KÍCH THƯỚC MPX";
+            this.smi_ktmpx.Click += new System.EventHandler(this.smi_ktmpx_Click);
+            // 
+            // smi_dll
+            // 
+            this.smi_dll.Name = "smi_dll";
+            this.smi_dll.Size = new System.Drawing.Size(172, 22);
+            this.smi_dll.Text = "ĐỘ LỒI LÕM";
+            this.smi_dll.Click += new System.EventHandler(this.smi_dll_Click);
+            // 
+            // smi_dn
+            // 
+            this.smi_dn.Name = "smi_dn";
+            this.smi_dn.Size = new System.Drawing.Size(172, 22);
+            this.smi_dn.Text = "ĐỘ NGHIÊNG";
+            this.smi_dn.Click += new System.EventHandler(this.smi_dn_Click);
+            // 
+            // smi_mcdh
+            // 
+            this.smi_mcdh.Name = "smi_mcdh";
+            this.smi_mcdh.Size = new System.Drawing.Size(124, 20);
+            this.smi_mcdh.Text = "MẶT CẮT ĐỊA HÌNH";
+            this.smi_mcdh.Click += new System.EventHandler(this.smi_mcdh_Click);
+            // 
+            // smi_gck
+            // 
+            this.smi_gck.Name = "smi_gck";
+            this.smi_gck.Size = new System.Drawing.Size(111, 20);
+            this.smi_gck.Text = "GÓC CHE KHUẤT";
+            this.smi_gck.Click += new System.EventHandler(this.smi_gck_Click);
+            // 
+            // smi_pvphmp
+            // 
+            this.smi_pvphmp.Name = "smi_pvphmp";
+            this.smi_pvphmp.Size = new System.Drawing.Size(150, 20);
+            this.smi_pvphmp.Text = "PHẠM VI PHÁT HIỆN MP";
+            this.smi_pvphmp.Click += new System.EventHandler(this.smi_pvphmp_Click);
+            // 
+            // smi_help
+            // 
+            this.smi_help.Name = "smi_help";
+            this.smi_help.Size = new System.Drawing.Size(47, 20);
+            this.smi_help.Text = "HELP";
+            // 
+            // tc_mainTap
+            // 
+            this.tc_mainTap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tc_mainTap.Location = new System.Drawing.Point(12, 37);
+            this.tc_mainTap.Name = "tc_mainTap";
+            this.tc_mainTap.SelectedIndex = 0;
+            this.tc_mainTap.Size = new System.Drawing.Size(1253, 603);
+            this.tc_mainTap.TabIndex = 1;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1277, 652);
+            this.Controls.Add(this.tc_mainTap);
+            this.Controls.Add(this.ms_mainMenu);
+            this.MainMenuStrip = this.ms_mainMenu;
+            this.MinimumSize = new System.Drawing.Size(1293, 691);
+            this.Name = "Form1";
+            this.Text = "Rada Analyst";
+            this.ms_mainMenu.ResumeLayout(false);
+            this.ms_mainMenu.PerformLayout();
+            this.tc_mainTap.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            //WELCOME PAGE
+            addWelcomeTabPage();
+        }
+
+        #endregion
+
+        private MenuStrip ms_mainMenu;
+        private ToolStripMenuItem smi_vtddrd;
+        private ToolStripMenuItem smi_kvctd;
+        private ToolStripMenuItem smi_dcdd;
+        private ToolStripMenuItem smi_mpx;
+        private ToolStripMenuItem smi_ktmpx;
+        private ToolStripMenuItem smi_dll;
+        private ToolStripMenuItem smi_dn;
+        private ToolStripMenuItem smi_mcdh;
+        private ToolStripMenuItem smi_gck;
+        private ToolStripMenuItem smi_pvphmp;
+        private ToolStripMenuItem smi_help;
+        
+        private TabControl tc_mainTap;
+        
+    }
+}
---
+++ RadarAnalyst/RadarAnalyst/Form1.cs
@@ -0,0 +1,136 @@
+namespace RadarAnalyst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addWelcomeTabPage()
+        {
+            TabPage tp_welcome;
+            Label lbl_appName;
+            PictureBox pictureBox1;
+            tp_welcome = new System.Windows.Forms.TabPage();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            lbl_appName = new System.Windows.Forms.Label();
+            tp_welcome.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
+            tc_mainTap.Controls.Add(tp_welcome);
+            // 
+            // tp_welcome
+            // 
+            tp_welcome.Controls.Add(pictureBox1);
+            tp_welcome.Controls.Add(lbl_appName);
+            tp_welcome.Location = new System.Drawing.Point(4, 24);
+            tp_welcome.Name = "tp_welcome";
+            tp_welcome.Size = new System.Drawing.Size(1245, 575);
+            tp_welcome.TabIndex = 0;
+            tp_welcome.Text = "Welcome";
+            tp_welcome.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = (System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom);
+            pictureBox1.Image = global::RadarAnalyst.Properties.Resources.radar;
+            pictureBox1.Location = new System.Drawing.Point((tc_mainTap.Width) / 5, 210);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(796, 350);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // lbl_appName
+            // 
+            lbl_appName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            lbl_appName.AutoSize = true;
+            lbl_appName.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            lbl_appName.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lbl_appName.Location = new System.Drawing.Point((tc_mainTap.Width) / 5, 50);
+            lbl_appName.Name = "lbl_appName";
+            lbl_appName.Size = new System.Drawing.Size(796, 76);
+            lbl_appName.TabIndex = 0;
+            lbl_appName.Text = "PHAN MEM \r\nTINH TOAN CAC THAM SO TRAN DIA RA DA PHONG KHONG";
+            lbl_appName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            tp_welcome.ResumeLayout(false);
+            tp_welcome.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
+            tc_mainTap.ContextMenuStrip = null;
+        }
+        private void addTabPage(string title)
+        {
+            TabPage myTabPage = new TabPage(title);
+            // add close button
+            ContextMenuStrip cms_close = new ContextMenuStrip();
+            ToolStripMenuItem tsmi_close = new ToolStripMenuItem();
+            tsmi_close.Text = "Close";
+            cms_close.Items.Add(tsmi_close);
+            tc_mainTap.ContextMenuStrip = cms_close;
+            tsmi_close.Click +=  new System.EventHandler(tsmi_close_Click);
+            // cant not open a new tab if it is up running
+            bool isAdded = false;
+            foreach (TabPage tp in tc_mainTap.TabPages)
+            {
+                if (tp.Text == title)
+                {
+                    MessageBox.Show("Cua so dang chay !", "Thong bao",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    isAdded = true;
+                    break;
+                }
+            }
+            if (!isAdded)
+            {
+                tc_mainTap.TabPages.Clear();
+                tc_mainTap.TabPages.Add(myTabPage);
+            }
+        }
+
+
+        private void tsmi_close_Click(object sender, EventArgs e)
+        {
+            tc_mainTap.TabPages.Remove(tc_mainTap.SelectedTab);
+        }
+
+        private void smi_kvctd_Click(object sender, EventArgs e)
+        {
+            addTabPage("KV CHON TD");
+        }
+
+        private void smi_dcdd_Click(object sender, EventArgs e)
+        {
+            addTabPage("Do Cao D. Dai");
+        }
+
+        private void smi_ktmpx_Click(object sender, EventArgs e)
+        {
+            addTabPage("Kich Thuoc MPX");
+        }
+
+        private void smi_dll_Click(object sender, EventArgs e)
+        {
+            addTabPage("Do Loi Lom");
+        }
+
+        private void smi_dn_Click(object sender, EventArgs e)
+        {
+            addTabPage("Do Nghieng");
+        }
+
+        private void smi_mcdh_Click(object sender, EventArgs e)
+        {
+            addTabPage("Mat Cat Dia Hinh");
+        }
+
+        private void smi_gck_Click(object sender, EventArgs e)
+        {
+            addTabPage("Goc Che Khuat");
+        }
+
+        private void smi_pvphmp_Click(object sender, EventArgs e)
+        {
+            addTabPage("Pham Vi Phat Hien MP");
+        }
+
+       
+    }
+}
